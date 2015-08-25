b5246007813d6e22046e6277a565c253620fd500
mrgeg <eugeneglnv@gmail.com>
2015-08-25 21:09:59 +0100

    sobol working

--- python/pyTest.py
+++ python/pyTest.py
@@ -3,14 +3,17 @@ import PyQoinSim
 
 random 	= PyQoinSim.RandomVarInt()
 
-x = random.getRandom(["TYPE:UniformMT"], 10000)
-y = random.getRandom(["TYPE:UniformMT"], 10000)
-
-plt.scatter(x,y)
-plt.show()
+x = random.getRandom(["TYPE:UniformMT"], 1100)
+y = random.getRandom(["TYPE:UniformMT"], 1100)
 
 x1 = random.getRandom(["TYPE:UniformSobol","SOBOLDIM:1100"], 1100)
 y1 = random.getRandom(["TYPE:UniformSobol","SOBOLDIM:1100"], 1100)
 
-plt.scatter(x1,y1)
+fig = plt.figure()
+axe = fig.add_subplot(111);
+
+axe.scatter(x,y, s=10, c='b', marker="s", label='Mersenne Twister')
+axe.scatter(x1,y1, s=10, c='r', marker="o", label='Sobol1111')
+
+plt.legend(loc='upper left');
 plt.show()
